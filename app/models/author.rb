9a487a1479f92cf15e10303a3586a06b1da0c65f
Rob <robert.gentile@powerhrg.com>
2021-09-10 09:39:58 -0400

    completed assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  validates :name, presence: true, uniqueness: true 
+  #uniqueness allows no two authors to have the same name
+  validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    "Won't Believe",
+    "Secret",
+    "Top [number]",
+    "Guess"
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| title&.match pat }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
